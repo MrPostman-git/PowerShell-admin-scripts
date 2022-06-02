3609a2951f355f638cc4bacdaf3ac64c6fc406af
MrPostman-git <sullyvann.d@protonmail.com>
2022-06-02 02:57:53 +0200

    add script

---
+++ Get_Disabled_Accounts.ps1
@@ -0,0 +1,142 @@
+﻿##################################################################
+# Help                                                                                                        #
+##################################################################
+<#  
+    Nom: Get_Disabled_accounts.ps1
+    Descripttion: 
+    Get Disabled Accounts, sort them by OU and Export by Excel or CSV
+    .EXAMPLE
+    Get_Disabled_Accounts.ps1
+    Version 1.3
+#>
+##################################################################
+# Main                                                                                                       #
+##################################################################
+# Get file path 
+$RootFolder = Split-Path -Path $MyInvocation.MyCommand.Definition
+# Test if active directory module is installed
+if((Get-Module ActiveDirectory) -eq $null){
+    try{
+        Import-Module ActiveDirectory
+    }catch{
+        Write-Host "The execution computer doesn't have ActiveDirectory Powershell Module. The script can't continue." -ForegroundColor Red
+        return
+    }
+}
+# Check if excel is installed
+try{
+    $objExcel = new-object -comobject excel.application
+    Write-Host "Excel is installed on this Computer, disabled Users will be export in a fashioned excel file."
+    $ExcelTest = $true
+}catch{
+    Write-Host "Excel is not installed on this Computer, disabled Users will be export in a plain old CSV file."
+    $ExcelTest = $false
+}
+# Get the current date for file
+$Date = Get-Date -Format ddMMyyyy
+# check if excel is available
+if($ExcelTest){
+    # set path of excel file
+    Write-Host "Create Excel file"
+    $ExcelPath = "$RootFolder\DisabledAccounts_$Date.xlsx"
+    if (Test-Path $ExcelPath) {
+        $finalWorkBook = $objExcel.WorkBooks.Open($ExcelPath)
+       # select the worksheet we working on 
+        $finalWorkSheet = $finalWorkBook.Worksheets.Item(1)
+        # set a name for the worksheet 
+        $finalWorkBook.Worksheets.Item(1).Name = "DisabledAccounts"
+    }else{
+        # creation of a new file
+        $finalWorkBook = $objExcel.Workbooks.Add()
+        $finalWorkSheet = $finalWorkBook.Worksheets.Item(1)
+        $finalWorkBook.Worksheets.Item(1).Name = "DisabledAccounts"
+    }
+    #$objExcel.Visible =$true
+    Write-Host "Create header"
+   # write the first line
+    $finalWorkSheet.Cells.Item(1,1) = "SamAccountName"
+    # set the text to Gras
+    $finalWorkSheet.Cells.Item(1,1).Font.Bold = $True
+    $finalWorkSheet.Cells.Item(1,2) = "FirstName";
+    $finalWorkSheet.Cells.Item(1,2).Font.Bold = $True
+    $finalWorkSheet.Cells.Item(1,3) = "LastName"
+    $finalWorkSheet.Cells.Item(1,3).Font.Bold = $True
+    $finalWorkSheet.Cells.Item(1,4) = "LastLogonDate"
+    $finalWorkSheet.Cells.Item(1,4).Font.Bold = $True
+    $finalWorkSheet.Cells.Item(1,5) = "DistinguishedName"
+    $finalWorkSheet.Cells.Item(1,5).Font.Bold = $True
+}else{
+    # Creation of the CSV file
+    $CSVPath = "$RootFolder\DisabledAccounts_$Date.csv"
+    # Creation of the header
+    $Header = "SamAccountName;FirstName;LastName;LastLogonDate;DistinguishedName"
+    # writing of header
+    $Header | Out-File -FilePath $CSVPath
+}
+Write-Host "Rrieving data..." -ForegroundColor Green
+# getting all the disbled accounts
+$ListUser = Get-ADUser -Filter {Enabled -eq $false} -Properties * | Select CanonicalName, CN, DistinguishedName, SamAccountName, GivenName, SurName, LastLogonDate, `
+@{Name="OU";Expression={(($_.CanonicalName).Substring($_.CanonicalName.IndexOf("/")+1)).replace("/$($_.CN)","")}} `
+| Sort-Object OU
+Write-Host "Writing data..." -ForegroundColor Green
+# we start at the second line (the first is for the Header)
+$FinalExcelRow = 2
+# select of a color for highlight
+$ColorIndex = 41
+# getting the OU of the first user
+if($ListUser.Count -gt 0){
+    $OU = $ListUser[0].OU
+}
+$i = 0
+#loop on every users
+ForEach($User in $ListUser){
+    #progress bar for every users
+    $PercentComplete = [System.Math]::Round($($i*100/($ListUser.Count)),2)
+    Write-Progress -Activity "Exporting data to Excel" -status "Effectué : $PercentComplete %" -percentcomplete $($i*100/($ListUser.Count))
+    $i++
+    if($ExcelTest){
+        # if OU has changed we change the color
+        if($OU -ne $User.OU){
+            $ColorIndex++
+            if($ColorIndex -ge 56){
+                $ColorIndex = 3
+            }
+        }
+       # getting the OU of the current user
+        $OU = $User.OU
+        #Store the differents values
+        $finalWorkSheet.Cells.Item($FinalExcelRow,1) = $User.SamAccountName
+        #attribute each different color for the values
+        $finalWorkSheet.Cells.Item($FinalExcelRow,1).Interior.ColorIndex = $ColorIndex
+        $finalWorkSheet.Cells.Item($FinalExcelRow,2) = $User.GivenName
+        $finalWorkSheet.Cells.Item($FinalExcelRow,2).Interior.ColorIndex = $ColorIndex
+        $finalWorkSheet.Cells.Item($FinalExcelRow,3) = $User.SurName
+        $finalWorkSheet.Cells.Item($FinalExcelRow,3).Interior.ColorIndex = $ColorIndex
+        $finalWorkSheet.Cells.Item($FinalExcelRow,4) = $User.LastLogonDate
+        $finalWorkSheet.Cells.Item($FinalExcelRow,4).Interior.ColorIndex = $ColorIndex
+        $finalWorkSheet.Cells.Item($FinalExcelRow,5) = $User.DistinguishedName
+        $finalWorkSheet.Cells.Item($FinalExcelRow,5).Interior.ColorIndex = $ColorIndex
+       $FinalExcelRow++
+    }else{
+        $Result = $User.SamAccountName+";"+$User.GivenName+";"+ `                            $User.SurName+ ";"+$User.LastLogonDate+";"+$User.DistinguishedName
+        $Result | Out-File -FilePath $CSVPath -Append 
+    } 
+}
+Write-Host "Saving data and closing Excel." -ForegroundColor Green
+if($ExcelTest){
+    # Select used cells
+    $UR = $finalWorkSheet.UsedRange
+   # Auto adjust of the column size    
+    $null = $UR.EntireColumn.AutoFit()
+    if (Test-Path $ExcelPath) {
+        # we save if the file exist
+        $finalWorkBook.Save()
+    }else{
+        # ifnot we give a name
+        $finalWorkBook.SaveAs($ExcelPath)
+    }
+    # closing the file
+    $finalWorkBook.Close()
+}
+# The Excel process used to process the transaction is terminated
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($objExcel)
---
+++ List_AD_SCCM_Computers.ps1
@@ -0,0 +1,209 @@
+﻿################################################################################################################################
+#                                                                                                                              #
+################################################################################################################################
+<#                                                                                                                             #
+##                                                                                                                             #
+##   Name: List_AD_SCCM_Computers                                                                                              #
+##   Description:                                                                                                              #
+##   Get ADComputer && Get CM-Device                                                                                           #
+##   .DESCRIPTION                                                                                                              #
+##   Récupérer les postes de SCCM et de l'AD, Les classe dans l'ordre et l'exporte en deux colonnes                            #
+     dans un fichier Excel ou CSV                                                                                              #
+                                                                                                                               #
+     Un AD et configuration Manager est nécéssaire pour l'utilisation de ce script                                             #
+     modifier la variable SCCM_Server_Name par le nom de votre serveur                                                         #
+##                                                                                                                             #
+##   Version : 1.3                                                                                                             #
+##   Auteur : Mr Postman                                                                                                       #
+##                                                                                                                             #
+##                                                                                                                             #
+##                                                                                                                             #
+##                                                                                                                             #
+#>                                                                                                                             # 
+################################################################################################################################
+#                                                                                                                              #
+################################################################################################################################
+# Récupération du chemin du fichier du script
+$RootFolder = Split-Path -Path $MyInvocation.MyCommand.Definition
+
+
+# Test de la présence du module Active Directory
+if((Get-Module ActiveDirectory) -eq $null){
+    try{
+        Import-Module ActiveDirectory
+    }catch{
+        Write-Host "The execution computer doesn't have ActiveDirectory Powershell Module. The script can't continue." -ForegroundColor Red
+        return
+    }
+}
+
+
+# Test de la présence du module Configuration Manager
+if((Get-Module ConfigurationManager) -eq $null){
+    try{
+        Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+    }catch{
+        Write-Host "The execution computer doesn't have the Configuration Manager Powershell Module. The script can't continue." -ForegroundColor Red
+        return
+    }
+}
+
+
+# Création de l'objet application Excel sinon on réalise un export au format CSV
+try{
+    $objExcel = new-object -comobject excel.application
+    Write-Host "Excel is installed on this Computer, a list of all the computers will be export in a fashioned excel file."
+    $ExcelTest = $true
+}catch{
+    Write-Host "Excel is not installed on this Computer, a list of all the computers will be export in a plain old CSV file."
+    $ExcelTest = $false
+}
+# Génération de la date du jour pour le nom du fichier d'export
+$Date = Get-Date -Format ddMMyyyy
+# Si Excel est disponible
+if($ExcelTest){
+    # Génération du chemin du fichier d'export
+    Write-Host "Create Excel file"
+    $ExcelPath = "$RootFolder\ListComputers_$Date.xlsx"
+   # Si le fichier Existe on l'ouvre
+    if (Test-Path $ExcelPath) {
+        $finalWorkBook = $objExcel.WorkBooks.Open($ExcelPath)
+       # On choisi l'onglet sur lequel on travaille
+        $finalWorkSheet = $finalWorkBook.Worksheets.Item(1)
+        # Donne un nom au à l'onglet
+        $finalWorkBook.Worksheets.Item(1).Name = "ListComputers"
+    }else{
+        # Création d'un nouveau fichier
+        $finalWorkBook = $objExcel.Workbooks.Add()
+        $finalWorkSheet = $finalWorkBook.Worksheets.Item(1)
+        $finalWorkBook.Worksheets.Item(1).Name = "ListComputers"
+    }
+    #$objExcel.Visible =$true
+    Write-Host "Create header"
+   # Rempli la première ligne
+    $finalWorkSheet.Cells.Item(1,1) = "Nom postes AD"
+    # Met le texte en gras
+    $finalWorkSheet.Cells.Item(1,1).Font.Bold = $True
+
+    $finalWorkSheet.Cells.Item(1,3) = "Nom postes SCCM"
+    $finalWorkSheet.Cells.Item(1,3).Font.Bold = $True
+    
+}else{
+    # Création du chemin du fichier CSV
+    $CSVPath = "$RootFolder\DisabledAccounts_$Date.csv"
+    # Création du header du fichier CSV
+    $Header = "Nom  Postes AD;LastLogonDate;LastName;FirstName;DistinguishedName"
+    # Ecriture du header
+    $Header | Out-File -FilePath $CSVPath
+}
+Write-Host "Rrieving AD computers..." -ForegroundColor Green
+# Récupération des ordianateur de l'AD
+$ListADComputer = Get-ADComputer -Filter * -Property * | Sort-Object Name -Descending |
+Select-Object Name | 
+Where-Object {$_.name -NotMatch "-TL" -And $_.name -NotMatch "VIRT" -And $_.name -NotMatch "SERV"-And $_.name -NotMatch "SRV" -And $_.name -NotMatch "PCL"}
+Write-Host "Writing data..." -ForegroundColor Green
+# On commence à la seconde ligne (la 1ère est consacrée au Header)
+$FinalExcelRow = 2
+# Choix d'une couleur pour surligner la ligne
+$ColorIndex = 00
+
+
+$i = 0
+#On boucle sur chaque ordianateur
+ForEach($Computer in $ListADComputer){
+    #On affiche une barre de progression montrant le nombre d'ordianateur déjà traités
+    $PercentComplete = [System.Math]::Round($($i*100/($ListADComputer.Count)),2)
+    Write-Progress -Activity "Exporting AD computers to Excel" -status "Effectué : $PercentComplete %" -percentcomplete $($i*100/($ListADComputer.Count))
+    $i++
+    
+    
+        if($ExcelTest){
+      
+        #On stocke les différentes valeurs
+        $finalWorkSheet.Cells.Item($FinalExcelRow,1) = $Computer.Name
+        #On attribut la couleur définit plus haut pour la case concerné
+        $finalWorkSheet.Cells.Item($FinalExcelRow,1).Interior.ColorIndex = $ColorIndex
+        
+        
+        # On incrémente le numéro de la ligne en cours d'écriture
+       $FinalExcelRow++
+    }else{
+        $Result = $Computer.Name+";"+$Computer.GivenName+";"+ `                            $Computer.SurName+ ";"+$Computer.LastLogonDate+";"+$Computer.DistinguishedName
+        $Result | Out-File -FilePath $CSVPath -Append 
+    } 
+} 
+
+
+
+#Affiche les postes SCCM
+
+# on importe le module de ConfigurationManager.psd1
+#Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+
+
+#Connexion au lecteur du site
+$SCCM_Server_Name='SRV-SCCM-PRI'
+New-PSDrive -Name 'PRI' -PSProvider CMSite -Root $SCCM_Server_Name| Out-Null
+
+
+#on définie l'emplacement actuelle par le code du site 
+Set-Location 'PRI:\'
+
+
+Write-Host "Rrieving SCCM computers..." -ForegroundColor Green
+# Récupération des ordianateur d'SCCM
+$ListSCCMComputer = Get-CMDevice  | Sort-Object Name -Descending | Select-Object Name, LastLogonDate |
+Where-Object {$_.name -NotMatch "VIRT" -And $_.name -NotMatch "SERV"-And $_.name -NotMatch "SRV" -And $_.name -NotMatch "PCL"}
+Write-Host "Writing data..." -ForegroundColor Green
+# On commence à la seconde ligne (la 1ère est consacrée au Header)
+$FinalExcelRow = 2
+# Choix d'une couleur pour surligner la ligne
+$ColorIndex = 00
+
+
+$i = 0
+#On boucle sur chaque ordinateur
+ForEach($Computer in $ListSCCMComputer){
+    #On affiche une barre de progression montrant le nombre d'ordinateur déjà traités
+    $PercentComplete = [System.Math]::Round($($i*100/($ListSCCMComputer.Count)),2)
+    Write-Progress -Activity "Exporting SCCM computers to Excel" -status "Effectué : $PercentComplete %" -percentcomplete $($i*100/($ListSCCMComputer.Count))
+    $i++
+    
+    
+    
+        if($ExcelTest){
+       
+        #On stocke les différentes valeurs
+        $finalWorkSheet.Cells.Item($FinalExcelRow,3) = $Computer.Name
+        #On attribut la couleur définit plus haut pour la case concerné
+        $finalWorkSheet.Cells.Item($FinalExcelRow,3).Interior.ColorIndex = $ColorIndex
+        
+       
+        # On incrémente le numéro de la ligne en cours d'écriture
+       $FinalExcelRow++
+    }else{
+        $Result = $Computer.Name+";"+$Computer.GivenName+";"+ `                            $Computer.SurName+ ";"+$Computer.LastLogonDate+";"+$Computer.DistinguishedName
+        $Result | Out-File -FilePath $CSVPath -Append 
+    } 
+ 
+}
+Write-Host "Saving data and closing Excel." -ForegroundColor Green
+if($ExcelTest){
+    # Sélectionne les cellules utilisées
+    $UR = $finalWorkSheet.UsedRange
+   # Auto ajustement de la taille de la colonne    
+    $null = $UR.EntireColumn.AutoFit()
+    if (Test-Path $ExcelPath) {
+        # Si le fichier existe déjà, on le sauvegarde
+        $finalWorkBook.Save()
+    }else{
+        # Sinon on lui donne un nom de fichier au moment de la sauvegarde
+        $finalWorkBook.SaveAs($ExcelPath)
+    }
+    # On ferme le fichier
+    $finalWorkBook.Close()
+}
+# Le processus Excel utilisé pour traiter l'opération est arrêté
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($objExcel)
+
+Set-Location $RootFolder
